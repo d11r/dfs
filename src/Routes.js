2ea0b98b45686ee5fe02fe56e3562021f51265cb
Dragos Strugar <itsdragos@gmail.com>
2019-11-10 23:15:06 +0300

    feat: add necessary routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,5 +5,16 @@ import ClientController from "./controllers/ClientController";
 const router = Express.Router();
 
 router.get("/ping", ClientController.pong);
+router.post("/init", ClientController.initialize);
+router.post("/touch", ClientController.createEmptyFile);
+router.get("/file", ClientController.readFile);
+router.post("/file", ClientController.writeFile);
+router.delete("/file", ClientController.deleteFile);
+router.get("/fileinfo", ClientController.getFileInfo);
+router.post("/filecopy", ClientController.copyFile);
+router.post("/filemove", ClientController.moveFile);
+router.get("/ls", ClientController.openDirectory);
+router.post("/mkdir", ClientController.makeDirectory);
+router.delete("/dir", ClientController.deleteDirectory);
 
 export default router;
--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -3,4 +3,78 @@ const pong = (req, res, next) => {
   next();
 };
 
-export default { pong };
+const initialize = (req, res, next) => {
+  res.send("todo: initialize");
+  next();
+};
+
+const createEmptyFile = (req, res, next) => {
+  res.send("todo: create empty file");
+  next();
+};
+
+const readFile = (req, res, next) => {
+  res.send("todo: read file");
+  next();
+};
+
+const writeFile = (req, res, next) => {
+  res.send("todo: write file");
+  next();
+};
+
+const deleteFile = (req, res, next) => {
+  res.send("todo: delete file");
+  next();
+};
+
+const getFileInfo = (req, res, next) => {
+  res.send("todo: get file info");
+  next();
+};
+
+const copyFile = (req, res, next) => {
+  res.send("todo: copy file");
+  next();
+};
+
+const moveFile = (req, res, next) => {
+  res.send("todo: move file");
+  next();
+};
+
+const openDirectory = (req, res, next) => {
+  res.send("todo: open directory (cd)");
+  next();
+};
+
+const readDirectory = (req, res, next) => {
+  res.send("todo: return list of files stored in directory");
+  next();
+};
+
+const makeDirectory = (req, res, next) => {
+  res.send("todo: mkdir");
+  next();
+};
+
+const deleteDirectory = (req, res, next) => {
+  res.send("todo: delete dir");
+  next();
+};
+
+export default {
+  pong,
+  initialize,
+  createEmptyFile,
+  readFile,
+  writeFile,
+  deleteFile,
+  getFileInfo,
+  copyFile,
+  moveFile,
+  openDirectory,
+  readDirectory,
+  makeDirectory,
+  deleteDirectory
+};
