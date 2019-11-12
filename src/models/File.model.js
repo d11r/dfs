19664975111127cb531f395b51efe6f46d7889e1
Dragos Strugar <itsdragos@gmail.com>
2019-11-12 18:30:00 +0300

    feat: add file metadata

--- src/models/File.model.js
+++ src/models/File.model.js
@@ -6,7 +6,8 @@ const FileSchema = new Schema({
   name: { type: String, required: true, max: 100, trim: true },
   hash: { type: String, required: true, maxlength: 64, minlength: 64 },
   path: { type: String, required: true, trim: true },
-  storages: [Schema.Types.ObjectId]
+  storages: [Schema.Types.ObjectId],
+  metadata: { type: String, required: true }
 });
 
 export default Mongoose.model("File", FileSchema);
